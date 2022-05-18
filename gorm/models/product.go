da3c9d3507a07a113d34ae80df610451b53a73ba
Daniel Suhett <danysuhett@hotmail.com>
2022-05-17 22:44:55 -0300

    struct to json

--- gorm/models/product.go
+++ gorm/models/product.go
@@ -1,9 +1,9 @@
 package models
 
 type Product struct {
-	Id          int
-	Name        string
-	Description string
-	Price       float64
-	Quantity    int
+	Id          int     `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float64 `json:"price"`
+	Quantity    int     `json:"quantity"`
 }
