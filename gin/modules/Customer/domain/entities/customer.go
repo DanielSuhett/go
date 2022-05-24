3e4b124b57e84a73f48a17513e8bde36d627f039
Daniel Suhett <danysuhett@hotmail.com>
2022-05-23 22:39:41 -0300

    update entities

--- gin/modules/Customer/domain/entities/customer.go
+++ gin/modules/Customer/domain/entities/customer.go
@@ -1,12 +1,18 @@
 package entities
 
 import (
-	"github.com/google/uuid"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type Customer struct {
-	ID uuid.UUID
-	Name string
-	Email string
+	ID        primitive.ObjectID `bson:"_id,omitempty"`
+	Name      string
+	Email     string
 	Addresses []Address
+}
+
+type CreateCustomer struct {
+	Name    string  `form:"name" binding:"required"`
+	Email   string  `form:"email" binding:"required"`
+	Address Address `form:"address" binding:"omitempty"`
 }
